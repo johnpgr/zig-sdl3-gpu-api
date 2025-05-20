7e66749fb94e5bb3f9deee93e6f7986bc5712dcb
johnpgr <jp.greidinger@hotmail.com>
2025-05-20 00:59:26 -0300

    update

--- build.zig
+++ build.zig
@@ -16,38 +16,106 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const shader_step = b.step("shaders", "Compile shaders");
-
-    const shader_source_dir = "assets/shaders/source";
-    const shader_out_dir = "assets/shaders/compiled";
-
-    const shader_types = [_]struct { extension: []const u8 }{
-        .{ .extension = ".vert.hlsl" },
-        .{ .extension = ".frag.hlsl" },
-        .{ .extension = ".comp.hlsl" },
+    // Check if shadercross is available
+    const has_shadercross = blk: {
+        const check_result = std.ChildProcess.run(.{
+            .allocator = b.allocator,
+            .argv = &[_][]const u8{"which", "shadercross"},
+            .cwd = ".",
+        }) catch |err| {
+            std.log.debug("Failed to check for shadercross: {}", .{err});
+            break :blk false;
+        };
+        defer {
+            b.allocator.free(check_result.stderr);
+            b.allocator.free(check_result.stdout);
+        }
+        break :blk check_result.term.Exited == 0;
     };
 
-    const shader_output_formats = [_]struct {
-        extension: []const u8,
-    }{
-        .{ .extension = ".spv" },
-        .{ .extension = ".msl" },
-        .{ .extension = ".dxil" },
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Check if shadercross is available
+    const has_shadercross = blk: {
+        const check_result = std.ChildProcess.run(.{
+            .allocator = b.allocator,
+            .argv = &[_][]const u8{"which", "shadercross"},
+            .cwd = ".",
+        }) catch |err| {
+            std.log.debug("Failed to check for shadercross: {}", .{err});
+            break :blk false;
+        };
+        defer {
+            b.allocator.free(check_result.stderr);
+            b.allocator.free(check_result.stdout);
+        }
+        break :blk check_result.term.Exited == 0;
     };
 
-    var shader_dir = std.fs.cwd().openDir(shader_source_dir, .{ .iterate = true }) catch |err| {
-        std.log.err("Failed to open shader directory: {}", .{err});
-        return;
-    };
-    defer shader_dir.close();
+    const shader_step = if (has_shadercross) shader_setup: {
+        const step = b.step("shaders", "Compile shaders (requires shadercross)");
 
-    var shader_iter = shader_dir.iterate();
+        const shader_source_dir = "assets/shaders/source";
+        const shader_out_dir = "assets/shaders/compiled";
 
-    while (shader_iter.next() catch |err| {
-        std.log.err("Failed to iterate shader directory: {}", .{err});
-        return;
-    }) |entry| {
-        if (entry.kind != .file) continue;
+        const shader_types = [_]struct { extension: []const u8 }{
+            .{ .extension = ".vert.hlsl" },
+            .{ .extension = ".frag.hlsl" },
+            .{ .extension = ".comp.hlsl" },
+        };
+
+        const shader_output_formats = [_]struct {
+            extension: []const u8,
+        }{
+            .{ .extension = ".spv" },
+            .{ .extension = ".msl" },
+            .{ .extension = ".dxil" },
+        };
+
+        var shader_dir = std.fs.cwd().openDir(shader_source_dir, .{ .iterate = true }) catch |err| {
+            std.log.err("Failed to open shader directory: {}", .{err});
+            break :shader_setup step;
+        };
+        defer shader_dir.close();
+
+        var shader_iter = shader_dir.iterate();
+
+        while (shader_iter.next() catch |err| {
+            std.log.err("Failed to iterate shader directory: {}", .{err});
+            break :shader_setup step;
+        }) |entry| {
+            if (entry.kind != .file) continue;
+
+            for (shader_types) |shader_input_type| {
+                if (std.mem.endsWith(u8, entry.name, shader_input_type.extension)) {
+                    const output_file_basename = entry.name[0 .. entry.name.len - 5];
+
+                    for (shader_output_formats) |output_format| {
+                        const shader_cmd = b.addSystemCommand(&.{
+                            "shadercross",
+                            entry.name,
+                            "-o",
+                            b.fmt(
+                                "../{s}/{s}{s}",
+                                .{
+                                    std.fs.path.basename(shader_out_dir),
+                                    output_file_basename,
+                                    output_format.extension,
+                                },
+                            ),
+                        });
+                        shader_cmd.setCwd(b.path(shader_source_dir));
+                        step.dependOn(&shader_cmd.step);
+                    }
+                    break;
+                }
+            }
+        }
+        break :shader_setup step;
+    } else b.step("shaders", "Compile shaders (shadercross not found - skipping)");
+
+    // Make sure the executable is installed after the shader step
+    b.getInstallStep().dependOn(shader_step);
 
         // Check if file matches any shader extension
         for (shader_types) |shader_input_type| {
@@ -122,7 +190,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     // Make sure the executable is installed after the shader step
-    // b.getInstallStep().dependOn(shader_step);
+    b.getInstallStep().dependOn(shader_step);
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
--- src/main.zig
+++ src/main.zig
@@ -18,11 +18,193 @@ pub var global_background: *c.SDL_Texture = undefined;
 pub var global_sprite: *c.SDL_Texture = undefined;
 pub var global_positions: [NUM_SPRITES]c.SDL_FRect = undefined;
 pub var global_velocities: [NUM_SPRITES]c.SDL_FPoint = undefined;
+pub var current_effect_idx: i32 = 0;
 
-pub fn processEvents(window: *c.SDL_Window) void {
-    _ = window;
-    var event: c.SDL_Event = undefined;
-    while (c.SDL_PollEvent(&event)) {
+const FullscreenEffect = enum {
+    EFFECT_NONE,
+    EFFECT_GRAYSCALE,
+    EFFECT_CRT,
+    NUM_EFFECTS,
+};
+
+const FullscreenEffectData = struct {
+    name: [*:0]const u8,
+    dxil_shader_source: [*]const u8,
+    dxil_shader_source_len: u32,
+    msl_shader_source: [*]const u8,
+    msl_shader_source_len: u32,
+    spirv_shader_source: [*]const u8,
+    spirv_shader_source_len: u32,
+    num_samplers: i32,
+    num_uniform_buffers: i32,
+    shader: *c.SDL_GPUShader,
+    state: *c.SDL_GPURenderState,
+};
+
+const CRTEffectUniforms = struct {
+    texture_width: f32,
+    texture_height: f32,
+};
+
+pub const effects = [_]FullscreenEffectData{
+    .{
+        .name = "NONE",
+        .dxil_shader_source = null,
+        .dxil_shader_source_len = 0,
+        .msl_shader_source = null,
+        .msl_shader_source_len = 0,
+        .spirv_shader_source = null,
+        .spirv_shader_source_len = 0,
+        .num_samplers = 0,
+        .num_uniform_buffers = 0,
+        .shader = null,
+        .state = null,
+    },
+    .{
+        .name = "Grayscale",
+        .dxil_shader_source = null,
+        .dxil_shader_source_len = 0,
+        .msl_shader_source = null,
+        .msl_shader_source_len = 0,
+        .spirv_shader_source = null,
+        .spirv_shader_source_len = 0,
+        .num_samplers = 1,
+        .num_uniform_buffers = 0,
+        .shader = null,
+        .state = null,
+    },
+    .{
+        .name = "CRT monitor",
+        .dxil_shader_source = null,
+        .dxil_shader_source_len = 0,
+        .msl_shader_source = null,
+        .msl_shader_source_len = 0,
+        .spirv_shader_source = null,
+        .spirv_shader_source_len = 0,
+        .num_samplers = 1,
+        .num_uniform_buffers = 1,
+        .shader = null,
+        .state = null,
+    },
+};
+
+pub fn initEffects(device: *c.SDL_GPUDevice) !void {
+    for (effects) |*effect| {
+        if (std.mem.eql(u8, std.mem.span(effect.name), "NONE")) {
+            continue;
+        }
+
+        const shader_name = if (std.mem.eql(u8, std.mem.span(effect.name), "Grayscale"))
+            "assets/shaders/compiled/grayscale.frag.spv"
+        else if (std.mem.eql(u8, std.mem.span(effect.name), "CRT monitor"))
+            "assets/shaders/compiled/crt.frag.spv"
+        else
+            continue;
+
+        effect.shader = try loadShader(device, shader_name, @intCast(effect.num_samplers), @intCast(effect.num_uniform_buffers), 0, 0);
+    }
+}
+
+pub fn drawScene() void {
+    var position: *c.SDL_FRect = undefined;
+    var velocity: *c.SDL_FPoint = undefined;
+
+    _ = c.SDL_RenderTexture(global_renderer, global_background, null,null);
+
+    var i: i32 = 0;
+    while (i < NUM_SPRITES) : (i += 1) {
+        position = &global_positions[i];
+        velocity = &global_velocities[i];
+        position.x += velocity.x;
+        if ((position.x < 0) || (position.x >= (WINDOW_WIDTH - global_sprite.w))) {
+            velocity.x = -velocity.x;
+            position.x += velocity.x;
+        }
+        position.y += velocity.y;
+        if ((position.y < 0) || (position.y >= (WINDOW_HEIGHT - global_sprite.h))) {
+            velocity.y = -velocity.y;
+            position.y += velocity.y;
+        }
+        c.SDL_RenderTexture(global_renderer, global_sprite, null, position);
+    }
+}
+
+
+pub fn initGPURenderState() !void {
+    var formats: c.SDL_GPUShaderFormat = undefined;
+    var info: c.SDL_GPUShaderCreateInfo = undefined;
+    var desc: c.SDL_GPURenderStateDesc = undefined;
+
+    global_device = try getGpuDeviceForRenderer(global_renderer);
+
+    formats = c.SDL_GetGPUShaderFormats(global_device);
+    if (formats == c.SDL_GPU_SHADERFORMAT_INVALID) {
+        c.SDL_Log("Couldn't get supported shader formats: %s", c.SDL_GetError());
+        return error.ShaderFormatError;
+    }
+
+    for (effects, 0..) |*data, i| {
+        if (i == @intFromEnum(FullscreenEffect.EFFECT_NONE)) {
+            continue;
+        }
+
+        // Zero initialize the info struct
+        @memset(std.mem.asBytes(&info), 0);
+
+        if (formats & c.SDL_GPU_SHADERFORMAT_SPIRV != 0) {
+            info.format = c.SDL_GPU_SHADERFORMAT_SPIRV;
+            info.code = data.spirv_shader_source;
+            info.code_size = data.spirv_shader_source_len;
+            info.entrypoint = "main";
+        } else if (formats & c.SDL_GPU_SHADERFORMAT_DXIL != 0) {
+            info.format = c.SDL_GPU_SHADERFORMAT_DXIL;
+            info.code = data.dxil_shader_source;
+            info.code_size = data.dxil_shader_source_len;
+            info.entrypoint = "main";
+        } else if (formats & c.SDL_GPU_SHADERFORMAT_MSL != 0) {
+            info.format = c.SDL_GPU_SHADERFORMAT_MSL;
+            info.code = data.msl_shader_source;
+            info.code_size = data.msl_shader_source_len;
+            info.entrypoint = "main0";
+        } else {
+            c.SDL_Log("No supported shader format found");
+            return error.NoSupportedShaderFormat;
+        }
+
+        info.num_samplers = @intCast(data.num_samplers);
+        info.num_uniform_buffers = @intCast(data.num_uniform_buffers);
+        info.stage = c.SDL_GPU_SHADERSTAGE_FRAGMENT;
+
+        data.shader = c.SDL_CreateGPUShader(global_device, &info) orelse {
+            c.SDL_Log("Couldn't create shader: %s", c.SDL_GetError());
+            return error.ShaderCreationFailed;
+        };
+
+        // Zero initialize the desc struct
+        @memset(std.mem.asBytes(&desc), 0);
+        desc.fragment_shader = data.shader;
+        
+        data.state = c.SDL_CreateGPURenderState(global_renderer, &desc) orelse {
+            c.SDL_Log("Couldn't create render state: %s", c.SDL_GetError());
+            return error.RenderStateCreationFailed;
+        };
+
+        if (i == @intFromEnum(FullscreenEffect.EFFECT_CRT)) {
+            var uniforms: CRTEffectUniforms = undefined;
+            @memset(std.mem.asBytes(&uniforms), 0);
+            uniforms.texture_width = @floatFromInt(global_target.w);
+            uniforms.texture_height = @floatFromInt(global_target.h);
+            
+            if (c.SDL_SetGPURenderStateFragmentUniformData(data.state, 0, &uniforms, @sizeOf(CRTEffectUniforms)) == 0) {
+                c.SDL_Log("Couldn't set uniform data: %s", c.SDL_GetError());
+                return error.UniformDataError;
+            }
+        }
+    }
+}
+
+pub fn handleAppEvent(event: *c.SDL_Event) void {
+    while (c.SDL_PollEvent(event)) {
         switch (event.type) {
             c.SDL_EVENT_QUIT => {
                 global_running = false;
@@ -54,7 +236,7 @@ pub fn getGpuDeviceForRenderer(renderer: *c.SDL_Renderer) !*c.SDL_GPUDevice {
 
 pub fn main() !void {
     if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
-        c.SDL_LogError(c.SDL_LOG_CATEGORY_APPLICATION, "SDL_Init failed: %s", c.SDL_GetError());
+        c.SDL_Log("SDL_Init failed: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     }
     defer c.SDL_Quit();
@@ -86,8 +268,18 @@ pub fn main() !void {
 
     c.SDL_Log("GPU device: %s", c.SDL_GetGPUDeviceDriver(global_device));
 
+    try initEffects(global_device);
+    defer {
+        for (effects) |effect| {
+            if (effect.shader) |shader| {
+                c.SDL_ReleaseGPUShader(global_device, shader);
+            }
+        }
+    }
+
     while (global_running) {
-        processEvents(global_window);
+        var event: *c.SDL_Event = undefined;
+        handleAppEvent(&event);
 
         _ = c.SDL_SetRenderDrawColor(global_renderer, 0, 0, 0, 255);
         _ = c.SDL_RenderClear(global_renderer);
@@ -149,7 +341,7 @@ pub fn loadTexture(
     const file_to_load = if (path) |p| p else file;
 
     surface = c.SDL_LoadBMP(file_to_load) orelse {
-        c.SDL_LogError(c.SDL_LOG_CATEGORY_APPLICATION, "Couldn't load %s: %s", file_to_load, c.SDL_GetError());
+        c.SDL_Log("Couldn't load %s: %s", file_to_load, c.SDL_GetError());
         return error.LoadBMPFailed;
     };
     defer c.SDL_FreeSurface(surface);
@@ -183,9 +375,71 @@ pub fn loadTexture(
     }
 
     texture = c.SDL_CreateTextureFromSurface(renderer, surface) orelse {
-        c.SDL_LogError(c.SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture: %s", c.SDL_GetError());
+        c.SDL_Log("Couldn't create texture: %s", c.SDL_GetError());
         return error.TextureCreationFailed;
     };
 
     return texture.?;
+}
+
+pub fn loadShader(
+    device: *c.SDL_GPUDevice,
+    comptime shader_name: []const u8,
+    sampler_count: u32,
+    uniform_buffer_count: u32,
+    storage_buffer_count: u32,
+    storage_texture_count: u32,
+) !*c.SDL_GPUShader {
+    const stage: c.SDL_GPUShaderStage =
+        if (c.SDL_strstr(shader_name.ptr, ".vert") != null)
+            c.SDL_GPU_SHADERSTAGE_VERTEX
+        else if (c.SDL_strstr(shader_name.ptr, ".frag") != null)
+            c.SDL_GPU_SHADERSTAGE_FRAGMENT
+        else {
+            c.SDL_Log("Invalid shader stage for file: %s", &shader_name);
+            return error.InvalidShaderStage;
+        };
+
+    const backend_formats = c.SDL_GetGPUShaderFormats(device);
+    var format: c.SDL_GPUShaderFormat = c.SDL_GPU_SHADERFORMAT_INVALID;
+    var extension: []const u8 = undefined;
+    var entrypoint: []const u8 = "main";
+
+    if (backend_formats & c.SDL_GPU_SHADERFORMAT_SPIRV != 0) {
+        format = c.SDL_GPU_SHADERFORMAT_SPIRV;
+        extension = ".spv";
+    } else if (backend_formats & c.SDL_GPU_SHADERFORMAT_DXIL != 0) {
+        format = c.SDL_GPU_SHADERFORMAT_DXIL;
+        extension = ".dxil";
+    } else if (backend_formats & c.SDL_GPU_SHADERFORMAT_MSL != 0) {
+        format = c.SDL_GPU_SHADERFORMAT_MSL;
+        extension = ".msl";
+        entrypoint = "main0";
+    } else {
+        c.SDL_Log("Unrecognized shader format for file: %s", &shader_name);
+        return error.NoSupportedShaderFormats;
+    }
+
+    const shader_path = "assets/shaders/compiled/" ++ shader_name ++ extension;
+
+    var code_size: usize = 0;
+    const code = c.SDL_LoadFile(shader_path.ptr, &code_size) orelse {
+        c.SDL_Log("Failed to load shader file: %s", &shader_path);
+        return error.ShaderFileLoadFailed;
+    };
+    defer c.SDL_free(code);
+
+    return c.SDL_CreateGPUShader(device, &.{
+        .code = @ptrCast(code),
+        .code_size = code_size,
+        .entrypoint = entrypoint.ptr,
+        .format = format,
+        .stage = stage,
+        .num_samplers = sampler_count,
+        .num_uniform_buffers = uniform_buffer_count,
+        .num_storage_buffers = storage_buffer_count,
+        .num_storage_textures = storage_texture_count,
+    }) orelse {
+        return error.ShaderCreationFailed;
+    };
 }
